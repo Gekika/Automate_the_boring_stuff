985df2650a3620945bb61331f802f00bb5be17de
Gekika <gkkaranja2@gmail.com>
2024-01-20 16:56:21 +0300

    each step at a time

---
+++ Chapter_3.py
@@ -0,0 +1,17 @@
+# this is the function I created from my understanding
+def add(a,b):
+    multiply=a*b
+    return multiply
+
+a = 2
+b = 4
+tim = add(a,b)
+print(tim)
+
+
+# another function called hello 
+def hello(name):
+    print("Hello, "+ name)
+    
+name = input("Enter your name, ")
+hello(name)
